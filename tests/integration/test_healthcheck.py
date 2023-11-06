5ac68d11f5ab014687a5d9f66f72978a9aae7df7
Artyom Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-11-06 13:38:52 +0100

    fix pipeline

--- tests/integration/test_healthcheck.py
+++ tests/integration/test_healthcheck.py
@@ -5,7 +5,6 @@ from user_management.config import config
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
 async def test_healthcheck():
     async with AsyncClient() as client:
         response = await client.get(url=f"{config.WEBAPP_TEST_HOST}/healthcheck", timeout=20)
